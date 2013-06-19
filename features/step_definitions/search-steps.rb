56b8f83f234afd2a041e6217b794ae0dc5e931c2
Tim Abell <tim@timwise.co.uk>
2013-06-19 15:16:59 +0100

    Stub search steps from cucumber output.

---
+++ features/step_definitions/search-steps.rb
@@ -0,0 +1,15 @@
+Given(/^I have a contact "(.*?)" "(.*?)"$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I search for "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The results should include "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The results should not include "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
