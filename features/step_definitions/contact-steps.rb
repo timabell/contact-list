13c56f9386a7ebd72b2ddc979a1043966cac2f60
Tim Abell <tim@timwise.co.uk>
2013-06-19 16:55:57 +0100

    Added auto generated step definitions from cucumber

--- features/step_definitions/contact-steps.rb
+++ features/step_definitions/contact-steps.rb
@@ -69,3 +69,23 @@ end
 Then(/^The results should not include "(.*?)"$/) do |arg1|
     pending # express the regexp above with the code you wish you had
 end
+
+When(/^I add a contact "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", nil$/) do |arg1, arg2, arg3, arg4, arg5, arg6|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I add a contact "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", nill, nil$/) do |arg1, arg2, arg3, arg4, arg5|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I have a user "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I set the picture to be of a cat$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The cat picture should show when I view the contact$/) do
+  pending # express the regexp above with the code you wish you had
+end
