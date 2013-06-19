5d4a09d01da2b7ab4f513f664a0a19534588479c
Tim Abell <tim@timwise.co.uk>
2013-06-19 23:11:08 +0100

    Generated step definitions (again)

---
+++ features/step_definitions/contact-steps.rb
@@ -0,0 +1,35 @@
+Given(/^A contact must have a "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^A contact may have a "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I should be able to view a list of contacts$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I view a contact$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see their details$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I should be able to add a contact$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I should be able to edit a contact's details$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I should be able to remove a contact$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I want to search for a contact by last name$/) do
+    pending # express the regexp above with the code you wish you had
+end
