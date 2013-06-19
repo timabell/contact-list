df4a1eca281cbb5378647958f7fe95eb7c2e6e0a
Tim Abell <tim@timwise.co.uk>
2013-06-19 15:09:22 +0100

    Stub stepdefs generated by cucumber

---
+++ features/step_definitions/listing-steps.rb
@@ -0,0 +1,55 @@
+Given(/^I have some contacts$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I view the list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see all my contacts$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I have a contact$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I view the contact$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the contact details$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am looking at the contact list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I add a contact$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The contact should be added to the list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am viewing a contact$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I edit the details$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The new details are remembered$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I remove the contact$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The contact should not be in the list$/) do
+  pending # express the regexp above with the code you wish you had
+end
