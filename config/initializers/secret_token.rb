d7562c46661eb4af920d1872e0e293aebd795714
Tim Abell <tim@timwise.co.uk>
2013-06-19 11:55:27 +0100

    Blank rails app
    $ rails new contact_list
    (fixed up directory hierarchy)
    
    Merged .gitignore (github
     default + rails default, dropped .orig).
    Removed rails stub readme.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactList::Application.config.secret_key_base = '89752b6ec248d37dfa26b282c400089d84aa2a73a39bad4bab24af1f03f6c56dbe762f3d50c1e871b9c021dafafb78c0abe52146ec21f5e0b27abbca11d19d83'
