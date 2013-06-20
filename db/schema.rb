89c852fc980664cab388dd7321db1174259d4e08
Tim Abell <tim@timwise.co.uk>
2013-06-20 01:54:00 +0100

    Schema updated - Contact model
    $ bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130620004011) do
+
+  create_table "contacts", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "mobile"
+    t.string   "landline"
+    t.string   "twitter"
+    t.string   "photo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
