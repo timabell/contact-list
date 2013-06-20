479ea79bea649683f82c2f8754075bf25c29bd9b
Tim Abell <tim@timwise.co.uk>
2013-06-20 01:36:16 +0100

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ContactList::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'contacts#list'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
